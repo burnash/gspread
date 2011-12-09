44e6aa3d995f9dc3775364edf87b1fef7b019ef7
burnash <anton.burnashev@gmail.com>
2011-12-09 17:21:54 +0000

    dir restructuring

---
+++ tests/test.py
@@ -0,0 +1,18 @@
+import os
+import unittest
+import ConfigParser
+
+class ClientTest(unittest.TestCase):
+
+    def setUp(self):
+        config_filename = os.path.join(os.path.dirname(__file__), "account_credentials.cfg")
+        config = ConfigParser.ConfigParser()
+        config.readfp(open(config_filename))
+        self.config = config
+
+    def test_login(self):
+        print self.config.get('Google Account', 'email')
+
+
+if __name__ == '__main__':
+    unittest.main()
