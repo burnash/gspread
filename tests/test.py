404bc953a4a9a880fdba25ad20fbf93d850f55a8
burnash <anton.burnashev@gmail.com>
2011-12-09 23:27:54 +0000

    config

--- tests/test.py
+++ tests/test.py
@@ -7,13 +7,27 @@ import gspread
 class ClientTest(unittest.TestCase):
 
     def setUp(self):
-        config_filename = os.path.join(os.path.dirname(__file__), "account_credentials.cfg")
-        config = ConfigParser.ConfigParser()
-        config.readfp(open(config_filename))
-        self.config = config
+        creds_filename = "account_credentials.cfg"
+        try:
+            config_filename = os.path.join(os.path.dirname(__file__), creds_filename)
+            config = ConfigParser.ConfigParser()
+            config.readfp(open(config_filename))
+            self.email = config.get('Google Account', 'email')
+            self.password = config.get('Google Account', 'password')
+        except IOError:
+            msg = "Can't find %s for reading google account credentials. " \
+                  "You can create it from %s.example in tests/ directory."
+            raise Exception(msg % (creds_filename, creds_filename))
 
-    def test_login(self):
-        print self.config.get('Google Account', 'email')
+    def test_instantiate(self):
+        gc = gspread.Client(self.email, self.password)
+        self.assertTrue(isinstance(gc, gspread.Client))
+
+    def test_login_and_open_book(self):
+        gc = gspread.Client(self.email, self.password)
+        gc.login()
+        book = gc.open('test1')
+        self.assertTrue(isinstance(book, gspread.Book))
 
 
 if __name__ == '__main__':
