f63474dc1962ce63a1ccb935152e875c3b724eb0
burnash <anton.burnashev@gmail.com>
2011-12-11 18:54:49 +0000

    HTTPSession pulled into its own module

--- gspread/__init__.py
+++ gspread/__init__.py
@@ -1,7 +1,6 @@
-import urllib
-import urllib2
-
 from xml.etree import ElementTree
+
+from .httpsession import HTTPSession
 
 
 AUTH_SERVER = 'https://www.google.com'
@@ -15,46 +14,6 @@ def _ns(name):
 
 def _ns1(name):
     return '{%s}%s' % (SPREADSHEET_NS, name)
-
-
-class HTTPSession(object):
-    def __init__(self, headers=None):
-        self.headers = headers or {}
-
-    def request(self, method, url, data=None, headers=None):
-        if data and not isinstance(data, basestring):
-            data = urllib.urlencode(data)
-
-        req = urllib2.Request(url, data)
-
-        if method == 'put':
-            req.get_method = lambda: 'PUT'
-
-        request_headers = self.headers.copy()
-
-        if headers:
-            for k, v in headers.items():
-                if v is None:
-                    del request_headers[k]
-                else:
-                    request_headers[k] = v
-
-        for k, v in request_headers.items():
-            req.add_header(k, v)
-
-        return urllib2.urlopen(req)
-
-    def get(self, url, **kwargs):
-        return self.request('get', url, **kwargs)
-
-    def post(self, url, data=None, **kwargs):
-        return self.request('post', url, data=data, **kwargs)
-
-    def put(self, url, data=None, **kwargs):
-        return self.request('put', url, data=data, **kwargs)
-
-    def add_header(self, name, value):
-        self.headers[name] = value
 
 
 class Client(object):
---
+++ gspread/httpsession.py
@@ -0,0 +1,41 @@
+import urllib
+import urllib2
+
+class HTTPSession(object):
+    def __init__(self, headers=None):
+        self.headers = headers or {}
+
+    def request(self, method, url, data=None, headers=None):
+        if data and not isinstance(data, basestring):
+            data = urllib.urlencode(data)
+
+        req = urllib2.Request(url, data)
+
+        if method == 'put':
+            req.get_method = lambda: 'PUT'
+
+        request_headers = self.headers.copy()
+
+        if headers:
+            for k, v in headers.items():
+                if v is None:
+                    del request_headers[k]
+                else:
+                    request_headers[k] = v
+
+        for k, v in request_headers.items():
+            req.add_header(k, v)
+
+        return urllib2.urlopen(req)
+
+    def get(self, url, **kwargs):
+        return self.request('get', url, **kwargs)
+
+    def post(self, url, data=None, **kwargs):
+        return self.request('post', url, data=data, **kwargs)
+
+    def put(self, url, data=None, **kwargs):
+        return self.request('put', url, data=data, **kwargs)
+
+    def add_header(self, name, value):
+        self.headers[name] = value
