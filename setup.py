1cdc60268c608cc5ec3307eadc161858ab880162
burnash <anton.burnashev@gmail.com>
2011-12-12 14:18:13 +0000

    classifiers

--- setup.py
+++ setup.py
@@ -7,10 +7,22 @@ except ImportError:
 
 setup(
     name='gspread',
+    packages=['gspread'],
     description='Google Spreadsheets Python library',
     version='0.0.1',
-    author='burnash',
+    author='Anton Burnashev',
     url='https://github.com/burnash/gspread',
-    packages=['gspread'],
+    keywords=['spreadsheets', 'google-spreadsheets'],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "Topic :: Office/Business :: Financial :: Spreadsheet",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+        ],
     license='MIT'
     )
