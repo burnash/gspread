3d4955e5cc545b6beb80cc5b88357723b37153a7
burnash <anton.burnashev@gmail.com>
2011-12-17 23:44:02 +0000

    tests refactor

--- tests/test.py
+++ tests/test.py
@@ -4,34 +4,67 @@ import ConfigParser
 
 import gspread
 
-class ClientTest(unittest.TestCase):
+class GspreadTest(unittest.TestCase):
 
     def setUp(self):
-        creds_filename = "account_credentials.cfg"
+        creds_filename = "tests.config"
         try:
             config_filename = os.path.join(os.path.dirname(__file__), creds_filename)
             config = ConfigParser.ConfigParser()
             config.readfp(open(config_filename))
-            self.email = config.get('Google Account', 'email')
-            self.password = config.get('Google Account', 'password')
+            email = config.get('Google Account', 'email')
+            password = config.get('Google Account', 'password')
+            self.config = config
+            self.gc = gspread.login(email, password)
+
+            self.assertTrue(isinstance(self.gc, gspread.Client))
         except IOError:
             msg = "Can't find %s for reading google account credentials. " \
                   "You can create it from %s.example in tests/ directory."
             raise Exception(msg % (creds_filename, creds_filename))
 
-    def test_instantiate(self):
-        gc = gspread.Client(auth=(self.email, self.password))
-        self.assertTrue(isinstance(gc, gspread.Client))
 
-    def test_login_and_open_book(self):
-        gc = gspread.Client(auth=(self.email, self.password))
-        gc.login()
-        spreadsheet = gc.open('test1')
+class ClientTest(GspreadTest):
+    """Test for gspread.client"""
+    def test_open(self):
+        title = self.config.get('Spreadsheet', 'title')
+        spreadsheet = self.gc.open(title)
         self.assertTrue(isinstance(spreadsheet, gspread.Spreadsheet))
 
-        worksheet = spreadsheet.get_worksheet(0)
-        self.assertTrue(isinstance(worksheet, gspread.Worksheet))
+    def test_no_found_exeption(self):
+        noexistent_title = "Please don't use this phrase as a name of a sheet."
+        self.assertRaises(gspread.SpreadsheetNotFound,
+                          self.gc.open,
+                          noexistent_title)
+
+    def test_open_by_key(self):
+        key = self.config.get('Spreadsheet', 'key')
+        spreadsheet = self.gc.open_by_key(key)
+        self.assertTrue(isinstance(spreadsheet, gspread.Spreadsheet))
+
+    def test_open_by_url(self):
+        url = self.config.get('Spreadsheet', 'url')
+        spreadsheet = self.gc.open_by_url(url)
+        self.assertTrue(isinstance(spreadsheet, gspread.Spreadsheet))
+
+    def test_openall(self):
+        spreadsheet_list = self.gc.openall()
+        for s in spreadsheet_list:
+            self.assertTrue(isinstance(s, gspread.Spreadsheet))
 
 
-if __name__ == '__main__':
-    unittest.main()
+class SpreadsheetTest(GspreadTest):
+    """Test for gspread.Spreadsheet"""
+    def setUp(self):
+        super(SpreadsheetTest, self).setUp()
+        title = self.config.get('Spreadsheet', 'title')
+        self.spreadsheet = self.gc.open(title)
+
+    def test_sheet1(self):
+        sheet1 = self.spreadsheet.sheet1
+        self.assertTrue(isinstance(sheet1, gspread.Worksheet))
+
+    def test_get_worksheet(self):
+        sheet1 = self.spreadsheet.get_worksheet(0)
+        self.assertTrue(isinstance(sheet1, gspread.Worksheet))
+
