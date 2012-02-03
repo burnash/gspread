ef4039c1dd5058c2a0a067bc9aab952fba20a954
burnash <anton.burnashev@gmail.com>
2012-02-03 12:46:32 +0000

    eliminating redundancy

--- gspread/models.py
+++ gspread/models.py
@@ -180,11 +180,7 @@ class Worksheet(object):
 
     def _fetch_cells(self):
         feed = self.client.get_cells_feed(self)
-        cells_list = []
-        for elem in feed.findall(_ns('entry')):
-            cells_list.append(Cell(self, elem))
-
-        return cells_list
+        return [Cell(self, elem) for elem in feed.findall(_ns('entry'))]
 
     _cell_addr_re = re.compile(r'([A-Za-z]+)(\d+)')
     def get_int_addr(self, label):
@@ -196,6 +192,7 @@ class Worksheet(object):
                       Letter case is ignored.
 
         Example:
+
         >>> wks.get_int_addr('A1')
         (1, 1)
 
@@ -264,31 +261,13 @@ class Worksheet(object):
         for cell in cells:
             rows.setdefault(int(cell.row), []).append(cell)
 
-        rows_list = []
-        for r in sorted(rows.keys()):
-            rows_list.append(rows[r])
+        rows_list = [rows[r] for r in sorted(rows.keys())]
 
-        simple_rows_list = []
-        for r in rows_list:
-            simple_row = []
-            for c in r:
-                simple_row.append(c.value)
-            simple_rows_list.append(simple_row)
+        return [[cell.value for cell in row] for row in rows_list]
 
-        return simple_rows_list
-
-    def row_values(self, row):
-        """Returns a list of all values in a `row`.
-
-        Empty cells in this list will be rendered as :const:`None`.
-
-        """
+    def _list_values(self, index, cell_tuple, position):
         cells_list = self._fetch_cells()
-
-        cells = {}
-        for cell in cells_list:
-            if int(cell.row) == row:
-                cells[int(cell.col)] = cell
+        cells = dict(map(cell_tuple, filter(position, cells_list)))
 
         try:
             last_index = max(cells.keys())
@@ -302,26 +281,25 @@ class Worksheet(object):
 
         return vals
 
+    def row_values(self, row):
+        """Returns a list of all values in a `row`.
+
+        Empty cells in this list will be rendered as :const:`None`.
+
+        """
+        return self._list_values(row,
+                                 lambda cell: (cell.col, cell),
+                                 lambda cell: cell.row == row)
+
     def col_values(self, col):
         """Returns a list of all values in column `col`.
 
         Empty cells in this list will be rendered as :const:`None`.
 
         """
-        cells_list = self._fetch_cells()
-
-        cells = {}
-        for cell in cells_list:
-            if int(cell.col) == col:
-                cells[int(cell.row)] = cell
-
-        last_index = max(cells.keys())
-        vals = []
-        for i in range(1, last_index + 1):
-            c = cells.get(i)
-            vals.append(c.value if c else None)
-
-        return vals
+        return self._list_values(col,
+                                 lambda cell: (cell.row, cell),
+                                 lambda cell: cell.col == col)
 
     def update_acell(self, label, val):
         """Sets the new value to a cell.
@@ -455,7 +433,6 @@ class Worksheet(object):
         :param query: A text string or compiled regular expression.
         """
         return self._finder(filter, query)
-
 
 
 class Cell(object):
