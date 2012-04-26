5a520cd4e6f8011ad51b20395a2a27f8db928c72
Javier Candeira <javier@candeira.com>
2012-04-26 16:17:58 +1000

    Add numericise() and numericise_all() functions and tests to utils.py

--- gspread/utils.py
+++ gspread/utils.py
@@ -45,3 +45,45 @@ def _ds(elem):
     _indent(elem)
     return ElementTree.tostring(elem)
 
+def numericise(value, empty2zero=False):
+    """Returns a value that depends on the input string:
+        - Float if input can be converted to Float
+        - Integer if iinputt can be converted to integer
+        - Zero if the input string is empty and empty2zero flag is set
+        - The same input string, empty or not, otherwise.
+
+    Executable examples:
+
+    >>> numericise("faa")
+    'faa'
+    >>> numericise("3")
+    3
+    >>> numericise("3.1")
+    3.1
+    >>> numericise("", empty2zero=True)
+    0
+    >>> numericise("", empty2zero=False)
+    ''
+    >>> numericise("")
+    ''
+    >>>
+    """
+
+    try:
+        value = int(value)
+    except ValueError:
+        try: 
+            value = float(value)
+        except ValueError:
+            if value == "" and empty2zero == True: 
+                value = 0
+                
+    return value
+
+def numericise_all(input, empty2zero=False):
+    """Returns a list of numericised values from strings"""
+    return [numericise(s, empty2zero) for s in input]
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
