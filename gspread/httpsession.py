2213739a77f3e0dd5771f0f7f596565f24f0a7f9
Valentin v. Seggern <valentin.vonseggern@telekom.de>
2012-10-19 17:12:37 +0200

    Changed the HTTP connectivity from urllib to httlib
    This enables persistent http connections (Keep-Alive) and is hence
    slightly faster. Especially if you are behind a chain of web proxies and
    update a lot of cells.

--- gspread/httpsession.py
+++ gspread/httpsession.py
@@ -8,21 +8,22 @@ This module contains a class for working with http sessions.
 
 """
 
-
-try:
-    import urllib2 as request
-    from urllib import urlencode
-    from urllib2 import HTTPError
-except ImportError:
-    from urllib import request
-    from urllib.parse import urlencode
-    from urllib.error import HTTPError
+import httplib
+import urlparse
+from urllib import urlencode
 
 try:
     unicode
 except NameError:
     basestring = unicode = str
 
+class HTTPError(Exception):
+    def __init__(self, response):
+        self.code = response.status
+        self.response = response
+
+    def read(self):
+        return self.response.read()
 
 class HTTPSession(object):
     """Handles HTTP activity while keeping headers persisting across requests.
@@ -31,15 +32,22 @@ class HTTPSession(object):
     """
     def __init__(self, headers=None):
         self.headers = headers or {}
+        self.connections = {}
 
     def request(self, method, url, data=None, headers=None):
         if data and not isinstance(data, basestring):
             data = urlencode(data)
 
-        req = request.Request(url, data)
-
-        if method == 'put':
-            req.get_method = lambda: 'PUT'
+        # If we have data and Content-Type is not set, set it...
+        if data and not headers.get('Content-Type', None):
+            headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        # If connection for this scheme+location is not established, establish it.
+        uri = urlparse.urlparse(url)
+        if not self.connections.get(uri.scheme+uri.netloc):
+            if uri.scheme == 'https':
+                self.connections[uri.scheme+uri.netloc] = httplib.HTTPSConnection(uri.netloc)
+            else:
+                self.connections[uri.scheme+uri.netloc] = httplib.HTTPConnection(uri.netloc)
 
         request_headers = self.headers.copy()
 
@@ -50,22 +58,27 @@ class HTTPSession(object):
                 else:
                     request_headers[k] = v
 
-        for k, v in request_headers.items():
-            req.add_header(k, v)
+        self.connections[uri.scheme+uri.netloc].request(method, url, data, headers=request_headers)
+        response = self.connections[uri.scheme+uri.netloc].getresponse()
 
-        try:
-            return request.urlopen(req)
-        except HTTPError as e:
-            raise e
+        if response.status > 399:
+            raise HTTPError(response)
+        return response
 
     def get(self, url, **kwargs):
-        return self.request('get', url, **kwargs)
+        return self.request('GET', url, **kwargs)
 
-    def post(self, url, data=None, **kwargs):
-        return self.request('post', url, data=data, **kwargs)
+    def post(self, url, data=None, headers={}):
+        return self.request('POST', url, data=data, headers=headers)
 
     def put(self, url, data=None, **kwargs):
-        return self.request('put', url, data=data, **kwargs)
+        return self.request('PUT', url, data=data, **kwargs)
 
     def add_header(self, name, value):
         self.headers[name] = value
+
+    def close(self):
+        try:
+            self.connection.close()
+        except:
+            print 'Connection could not be closed.'
