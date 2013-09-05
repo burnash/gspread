a62c70e57d3b329c1a147ee76aa9b6441f5cb059
Martin 'Hasan' Bramwell <martinhbramwell@gmail.com>
2013-09-05 08:37:49 -0400

    Obfuscate private details

--- test_parms.py
+++ test_parms.py
@@ -6,8 +6,7 @@ google_project_id = '326xxxxxx42.apps.googleusercontent.com'
 google_redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
 secret = 'WpSlnpIjMxxxxxx15ejhN4hj'
 
-workbook_key = '0Ao882NWHcSjgdHJJSURUSE04QVhQRUxTbWJLOHJZT0E'
-# workbook_key = '0Asxy-TdDgbjidGd1cEtIbmhQblRyV1NIS3pfZ0F5MGc'
+workbook_key = '0Ao882NWHcSjgdHJJSUxxxxxxQVhQRUxTbWJLOHJZT0E'
 
 log_file_name = './logs/gOAuth.log'
 store_file = '.tknStore.db'
