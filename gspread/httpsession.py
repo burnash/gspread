6c7ae2265eea6e13b7b31ebc4e1c50cbd33b92d5
finoptimal-dev <developer@finoptimal.com>
2015-10-07 08:45:22 -0400

    MORE verbosity!

--- gspread/httpsession.py
+++ gspread/httpsession.py
@@ -109,8 +109,10 @@ class HTTPSession(object):
                 attempts += 1
                 time.sleep(1)
                 if self.tries > 1:
+                    print "Pre TB call."
                     print "Exception on try %d:" % attempts
                     import traceback;traceback.print_exc()
+                    print "Post TB call."
                 if attempts > 2:
                     print "Failed %d times...sleeping for 10 secs now." % \
                         attempts
