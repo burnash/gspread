c2d5ac44b3cf7d9fd6bc8e885e05e73a4a8932ac
burnash <anton.burnashev@gmail.com>
2018-04-08 19:44:53 +0200

    Remove API v3 (xml) related code from `gspread.utils`

--- gspread/utils.py
+++ gspread/utils.py
@@ -13,8 +13,6 @@ from functools import wraps
 from collections import defaultdict
 from itertools import chain
 
-from xml.etree import ElementTree
-
 from .exceptions import IncorrectCellLabel, NoValidUrlKeyFound
 
 MAGIC_NUMBER = 64
@@ -29,34 +27,6 @@ def finditem(func, seq):
 
     """
     return next((item for item in seq if func(item)))
-
-
-# http://stackoverflow.com/questions/749796/pretty-printing-xml-in-python
-# http://effbot.org/zone/element-lib.htm#prettyprint
-def _indent(elem, level=0):
-    i = "\n" + level * "  "
-    if len(elem):
-        if not elem.text or not elem.text.strip():
-            elem.text = i + "  "
-        if not elem.tail or not elem.tail.strip():
-            elem.tail = i
-        for elem in elem:
-            _indent(elem, level + 1)
-        if not elem.tail or not elem.tail.strip():
-            elem.tail = i
-    else:
-        if level and (not elem.tail or not elem.tail.strip()):
-            elem.tail = i
-
-
-def _ds(elem):
-    """ElementTree debug function.
-
-    Indents and renders xml tree to a string.
-
-    """
-    _indent(elem)
-    return ElementTree.tostring(elem)
 
 
 def numericise(value, empty2zero=False, default_blank=""):
