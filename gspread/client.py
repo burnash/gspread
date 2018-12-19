8d73388b8bd2c5b873fc425a0da6832d2ef20350
tucker <tucker1014@hotmail.com>
2018-12-19 10:36:42 -0800

    update docs
    Signed-off-by: tuckerrrrrrrrrrrr <tucker1014@hotmail.com>

--- gspread/client.py
+++ gspread/client.py
@@ -28,8 +28,8 @@ class Client(object):
     """An instance of this class communicates with Google API.
 
     :param auth: An OAuth2 credential object. Credential objects
-                 are those created by the oauth2client library.
-                 https://github.com/google/oauth2client
+                 are those created by the google-auth library.
+                 https://github.com/googleapis/google-auth-library-python
     :param session: (optional) A session object capable of making HTTP requests
                     while persisting some parameters across requests.
                     Defaults to `requests.Session <http://docs.python-requests.org/en/master/api/#request-sessions>`_.
