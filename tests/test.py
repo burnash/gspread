59ec44d7aa33361189f720c1dddc3f9e2c0997d5
Kirill Gashkov <gashkov.k.e@gmail.com>
2018-12-22 19:05:18 +0500

    Add test for Worksheet.sort() method

--- tests/test.py
+++ tests/test.py
@@ -497,6 +497,77 @@ class WorksheetTest(GspreadTest):
         self.assertEqual(grid_props['rowCount'], new_rows)
         self.assertEqual(grid_props['columnCount'], new_cols)
 
+    def test_sort(self):
+        rows = [
+            ["Apple", "2012", "4"],
+            ["Banana", "2013", "3"],
+            ["Canada", "2007", "1"],
+            ["Dinosaur", "2013", "6"],
+            ["Elephant", "2019", "2"],
+            ["Fox", "2077", "5"],
+        ]
+
+        self.sheet.resize(6, 3)
+        cell_list = self.sheet.range('A1:C6')
+        for c, v in zip(cell_list, itertools.chain(*rows)):
+            c.value = v
+        self.sheet.update_cells(cell_list)
+
+        specs = [
+            (3, 'asc'),
+        ]
+        self.sheet.sort(*specs, range='A1:C6')
+        rows = sorted(rows, key=lambda x: int(x[2]), reverse=False)
+        self.assertEqual(self.sheet.get_all_values(), rows)
+
+        specs = [
+            (1, 'des'),
+        ]
+        self.sheet.sort(*specs, range='A1:C6')
+        rows = sorted(rows, key=lambda x: x[0], reverse=True)
+        self.assertEqual(self.sheet.get_all_values(), rows)
+
+        specs = [
+            (2, 'asc'),
+            (3, 'asc'),
+        ]
+        self.sheet.sort(*specs, range='A1:C6')
+        rows = sorted(rows, key=lambda x: (x[1], int(x[2])), reverse=False)
+        self.assertEqual(self.sheet.get_all_values(), rows)
+
+        specs = [
+            (3, 'asc'),
+        ]
+        self.sheet.sort(*specs)
+        rows = sorted(rows, key=lambda x: int(x[2]), reverse=False)
+        self.assertEqual(self.sheet.get_all_values(), rows)
+
+        specs = [
+            (3, 'des'),
+        ]
+        self.sheet._properties['gridProperties']['frozenRowCount'] = 1
+        self.sheet.sort(*specs)
+        rows = [rows[0]] + sorted(rows[1:], key=lambda x: int(x[2]), reverse=True)
+        self.assertEqual(self.sheet.get_all_values(), rows)
+
+    def test_clear(self):
+        rows = [
+            ["", "", "", ""],
+            ["", "", "", ""],
+            ["A1", "B1", "", "D1"],
+            [1, "b2", 1.45, ""],
+            ["", "", "", ""],
+            ["A4", 0.4, "", 4],
+        ]
+
+        cell_list = self.sheet.range('A1:D6')
+        for cell, value in zip(cell_list, itertools.chain(*rows)):
+            cell.value = value
+        self.sheet.update_cells(cell_list)
+
+        self.sheet.clear()
+        self.assertEqual(self.sheet.get_all_values(), [])
+
     def test_find(self):
         sg = self._sequence_generator()
         value = next(sg)
