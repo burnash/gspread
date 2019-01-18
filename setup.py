dab4076b016d9a2dcc0d164ab0bea1099485f715
Hugo <hugovk@users.noreply.github.com>
2019-01-18 23:18:33 +0200

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
     url='https://github.com/burnash/gspread',
     keywords=['spreadsheets', 'google-spreadsheets'],
     install_requires=['requests>=2.2.1'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
