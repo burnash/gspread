22a6f2b1cb299c35ef28238c910a6b1fc773fe61
fabytm <36955933+fabytm@users.noreply.github.com>
2019-08-10 23:09:22 +0300

    Added append_rows method
    This method is used in order to add multiple rows at one time. Using this method overcomes the problem where the Google Sheets API limits your writes because the API has been called too many times in a short period of time.

--- gspread/models.py
+++ gspread/models.py
@@ -891,6 +891,29 @@ class Worksheet(object):
         }
 
         return self.spreadsheet.values_append(self.title, params, body)
+    
+        def append_rows(self, values, value_input_option='RAW'):
+        """Adds a row to the worksheet and populates it with values.
+        Widens the worksheet if there are more values than columns.
+
+        :param values: List of values for the new row.
+        :param value_input_option: (optional) Determines how input data should
+                                    be interpreted. See `ValueInputOption`_ in
+                                    the Sheets API.
+        :type value_input_option: str
+
+        .. _ValueInputOption: https://developers.google.com/sheets/api/reference/rest/v4/ValueInputOption
+
+        """
+        params = {
+            'valueInputOption': value_input_option
+        }
+
+        body = {
+            'values': values
+        }
+
+        return self.spreadsheet.values_append(self.title, params, body)
 
     def insert_row(
         self,
