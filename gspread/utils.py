96359a80edcf7b170977d074ffdb7ea0e1c077bc
Sergei Kaparis <43264989+skaparis@users.noreply.github.com>
2019-11-21 22:11:45 +0300

    Fix update_cell on non rect cell list (#613)

--- gspread/utils.py
+++ gspread/utils.py
@@ -230,8 +230,8 @@ def cell_list_to_rect(cell_list):
 
     rows = defaultdict(lambda: {})
 
-    row_offset = cell_list[0].row
-    col_offset = cell_list[0].col
+    row_offset = min(c.row for c in cell_list)
+    col_offset = min(c.col for c in cell_list)
 
     for cell in cell_list:
         row = rows.setdefault(int(cell.row) - row_offset, {})
