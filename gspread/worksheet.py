19a34897548602d36a9dcdb5584a97b7a2af9573
Marti Markov <1858736+martimarkov@users.noreply.github.com>
2021-01-20 00:11:01 +0200

    Add include_values_in_response to append_row & append_rows

--- gspread/worksheet.py
+++ gspread/worksheet.py
@@ -1057,6 +1057,7 @@ class Worksheet:
         value_input_option="RAW",
         insert_data_option=None,
         table_range=None,
+        include_values_in_response=False,
     ):
         """Adds a row to the worksheet and populates it with values.
 
@@ -1072,6 +1073,9 @@ class Worksheet:
         :param str table_range: (optional) The A1 notation of a range to search
             for a logical table of data. Values are appended after the last row
             of the table. Examples: ``A1`` or ``B2:D4``
+        :param bool include_values_in_response: (optional) Determines if the
+            update response should include the values of the cells that were
+            appended. By default, responses do not include the updated values.
 
         .. _ValueInputOption: https://developers.google.com/sheets/api/reference/rest/v4/ValueInputOption
         .. _InsertDataOption: https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/append#InsertDataOption
@@ -1082,6 +1086,7 @@ class Worksheet:
             value_input_option=value_input_option,
             insert_data_option=insert_data_option,
             table_range=table_range,
+            include_values_in_response=include_values_in_response,
         )
 
     def append_rows(
@@ -1090,6 +1095,7 @@ class Worksheet:
         value_input_option="RAW",
         insert_data_option=None,
         table_range=None,
+        include_values_in_response=False,
     ):
         """Adds multiple rows to the worksheet and populates them with values.
 
@@ -1106,6 +1112,9 @@ class Worksheet:
         :param str table_range: (optional) The A1 notation of a range to search
             for a logical table of data. Values are appended after the last row
             of the table. Examples: ``A1`` or ``B2:D4``
+        :param bool include_values_in_response: (optional) Determines if the
+            update response should include the values of the cells that were
+            appended. By default, responses do not include the updated values.
 
         .. _ValueInputOption: https://developers.google.com/sheets/api/reference/rest/v4/ValueInputOption
         .. _InsertDataOption: https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/append#InsertDataOption
@@ -1115,6 +1124,7 @@ class Worksheet:
         params = {
             "valueInputOption": value_input_option,
             "insertDataOption": insert_data_option,
+            "includeValuesInResponse": include_values_in_response,
         }
 
         body = {"values": values}
