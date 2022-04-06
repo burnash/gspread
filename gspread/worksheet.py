11b432a053066c48d4c4721e8d1f0cbe301b8918
Alexandre Lavigne <lavigne958@gmail.com>
2022-04-06 16:40:16 +0200

    Add expected headers argument
    Add a new argument to `get_all_records` to provide the list of expected
    headers.
    
    The given expected headers must:
    - be unique
    - be part of the complete headers list
    - must not contain extra headers
    
    This will provide a way for users to use this method and still
    have *some* duplicated headers that are not relevant to pull.
    
    This will ensure the columns that matters have unique headers.
    
    Closes #1007

--- gspread/worksheet.py
+++ gspread/worksheet.py
@@ -376,6 +376,7 @@ class Worksheet:
         allow_underscores_in_numeric_literals=False,
         numericise_ignore=None,
         value_render_option=None,
+        expected_headers=None,
     ):
         """Returns a list of dictionaries, all of them having the contents of
         the spreadsheet with the head row as keys and each of these
@@ -399,6 +400,7 @@ class Worksheet:
         :param str value_render_option: (optional) Determines how values should
             be rendered in the the output. See `ValueRenderOption`_ in
             the Sheets API.
+        :param list expected_headers: (optional) List of expected headers, they must be unique.
 
         .. _ValueRenderOption: https://developers.google.com/sheets/api/reference/rest/v4/ValueRenderOption
         """
@@ -412,9 +414,27 @@ class Worksheet:
 
         keys = data[idx]
 
-        # Check keys are uniques
-        if len(keys) != len(set(keys)):
-            raise GSpreadException("headers must be uniques")
+        # if no given expected headers, expect all of them
+        if expected_headers is None:
+            expected_headers = keys
+
+        # keys must:
+        # - be uniques
+        # - be part of the complete header list
+        # - not contain extra headers
+        expected = set(expected_headers)
+        headers = set(keys)
+
+        # make sure they are uniques
+        if len(expected) != len(expected_headers):
+            raise GSpreadException("the given 'expected_headers' are not uniques")
+
+        if not expected & headers == expected:
+            raise GSpreadException(
+                "the given 'expected_headers' contains unknown headers: {}".format(
+                    expected & headers
+                )
+            )
 
         if numericise_ignore == ["all"]:
             values = data[idx + 1 :]
