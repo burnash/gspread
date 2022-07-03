133e138c8a72ff01790b70b5768fa07a688005ed
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2022-07-03 09:04:32 -0400

    Fixed linting warning in `__init__.py`

--- gspread/__init__.py
+++ gspread/__init__.py
@@ -13,7 +13,13 @@ __version__ = "5.4.0"
 __author__ = "Anton Burnashev"
 
 
-from .auth import authorize, oauth, oauth_from_dict, service_account, service_account_from_dict
+from .auth import (
+    authorize,
+    oauth,
+    oauth_from_dict,
+    service_account,
+    service_account_from_dict,
+)
 from .cell import Cell
 from .client import BackoffClient, Client, ClientFactory
 from .exceptions import (
