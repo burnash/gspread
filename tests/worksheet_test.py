5f2af1f99d69791451048f9ef4cd9d3c55dfb4a5
alifeee <alifeeerenn@hotmail.com>
2023-06-06 23:29:35 +0100

    test manually computed colours

--- tests/worksheet_test.py
+++ tests/worksheet_test.py
@@ -209,6 +209,44 @@ class WorksheetTest(GspreadTest):
         self.assertEqual(color_param_after, pink_color_from_google)
 
     @pytest.mark.vcr()
+    def test_update_tab_color_generate_colors(self):
+        # test "manual generation of colors"
+        color = {
+            "red": 0.99,
+            "green": 0.24,
+            "blue": 0.01,
+        }
+        color_from_google = {
+            "red": 0.9882353,  # 253/255
+            "green": 0.23921569,  # 61/255
+            "blue": 0.007843138,  # 3/255
+        }
+
+        params = {"fields": "sheets.properties.tabColorStyle"}
+        res = self.spreadsheet.fetch_sheet_metadata(params=params)
+        color_before = (
+            res["sheets"][0]["properties"]
+            .get("tabColorStyle", {})
+            .get("rgbColor", None)
+        )
+        color_param_before = self.sheet.tab_color
+
+        self.sheet.update_tab_color(color)
+
+        res = self.spreadsheet.fetch_sheet_metadata(params=params)
+        color_after = (
+            res["sheets"][0]["properties"]
+            .get("tabColorStyle", {})
+            .get("rgbColor", None)
+        )
+        color_param_after = self.sheet.tab_color
+
+        self.assertEqual(color_before, None)
+        self.assertEqual(color_param_before, None)
+        self.assertEqual(color_after, color_from_google)
+        self.assertEqual(color_param_after, color_from_google)
+
+    @pytest.mark.vcr()
     def test_clear_tab_color(self):
         # Set the color.
         # Clear the color.
