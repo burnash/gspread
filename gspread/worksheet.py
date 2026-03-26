1e05bd53282d1963d3973f25b3dc92d24a1139cc
“antoineeripret” <antoine.eripret@gmail.com>
2026-03-26 23:16:43 +0100

    update_named_range() method added

--- gspread/worksheet.py
+++ gspread/worksheet.py
@@ -2923,6 +2923,31 @@ class Worksheet:
         }
         return self.client.batch_update(self.spreadsheet_id, body)
 
+    @cast_to_a1_notation
+    def update_named_range(self, named_range_id: str, new_name: str) -> JSONResponse:
+        """
+        :param str named_range_id: The ID of the named range to update.
+            Can be obtained with Spreadsheet.list_named_ranges()
+        :param str new_name: The new name to assign to the named range.
+
+        :returns: the response body from the request
+        :rtype: dict
+        """
+        body = {
+            "requests": [
+                {
+                    "updateNamedRange": {
+                        "namedRange": {
+                            "namedRangeId": named_range_id,
+                            "name": new_name,
+                        },
+                        "fields": "name",
+                    }
+                }
+            ]
+        }
+        return self.client.batch_update(self.spreadsheet_id, body)
+
     def _add_dimension_group(
         self, start: int, end: int, dimension: Dimension
     ) -> JSONResponse:
