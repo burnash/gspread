e94ad975c9f0e63462fb7a2122b2d7d559edef88
Ev2geny <ev2geny@gmail.com>
2026-07-13 18:39:26 +0200

    CONTRIBUTING.md is updated to differentiat between usage of Service Account cridentials and OAuth user credentions with the refernce to the procedure in the docs/oauth2.rst
    docs/oauth2.rst is updated to reflect the current (and more detailed)
    procedure to obtain OAuth user credentials

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,8 +31,9 @@ I18N_STR = "Iñtërnâtiônàlizætiøn"  # .encode('utf8')
 
 def read_credentials(filename: str) -> Credentials:
     # Support both a service-account key and an OAuth "authorized_user" file.
-    # Service accounts have no Drive storage quota and can't own files, so
-    # recording cassettes (which create spreadsheets) requires OAuth user creds.
+    # Unless in Google Workspace environment, service accounts have no Drive
+    # storage quota and can't own files, so running tests online
+    # (which create spreadsheets) requires OAuth user creds.
     # An OAuth user file has a "refresh_token"; a service-account key has a
     # "private_key" (and gspread's stored user file has no "type" field).
     with open(filename) as file:
